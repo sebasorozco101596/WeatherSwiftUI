45b8963cf8fb8c72dce201db8ec64345e5e6924b
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2021-12-03 23:38:19 -0500

    Initial Commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Juan Sebastian Orozco Buitrago on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
